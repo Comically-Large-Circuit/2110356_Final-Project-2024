c90f9308e357cc6b38ca66f356c60f95c8488344
Nithi .P <p51104.2015@gmail.com>
2024-11-21 17:21:14 +0700

    add blynk connecting file

--- Final-Project-PIO/src/main.cpp
+++ Final-Project-PIO/src/main.cpp
@@ -9,102 +9,53 @@ Moist Meter: 15
 Ultrasonic Pin: trig-5 echo-18
 */
 
-//Light Meter
+// Light Meter
 BH1750 lightMeter;
 
-//Air Measure
+// Air Measure
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME280 bme; // I2C
 unsigned long delayTime;
 
-//Moist Meter
-int _moisture,sensor_analog;
-int sensor_pin = 15;  
+// Moist Meter
+int _moisture, sensor_analog;
+int sensor_pin = 15;
 
-//Ultrasonic
-const int trigPin = 5; 
-const int echoPin = 18; 
+// Ultrasonic
+const int trigPin = 5;
+const int echoPin = 18;
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
 long duration;
 float distanceCm;
 float distanceInch;
 
-
-void setup() {
-  Serial.begin(9600);
-
-  // Initialize the I2C bus for Light measuring
-  Wire.begin(21, 22); // SDA21, SCL=22
-  
-  //setup for air measuring
-  bool status;
-  status = bme.begin(0x76);
-  
-  //Light measurre init Log
-  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
-    //Serial.println(F("BH1750 initialized"));
-  } else {
-    Serial.println(F("Error initializing BH1750"));
-    while(1);
-  }
-
-  //Air measure error Log
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while(1);
-  }
-
-  //Ultrasonic Setup
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-
-  pinMode(25,OUTPUT);
-
-}
-
-void loop() {
-
-  //Print Light Value 
-  printLightValues();
-
-  //Print Air Value
-  printAirValues();
-
-  //Moist Meter 
-  printMoistMeter();
-
-  //Ultrasonic
-  printUltraSonicValues();
-
-  Serial.println();
-
-  // digitalWrite(25,HIGH);
-  delay(2000);
-}
-
-
-void printLightValues(){
+void printLightValues()
+{
   float lux = lightMeter.readLightLevel();
-  if (lux >= 0) {
+  if (lux >= 0)
+  {
     Serial.print("Light: ");
     Serial.print(lux);
     Serial.println(" lx");
-  } else {
+  }
+  else
+  {
     Serial.println("Error reading light level");
   }
 }
 
-void printAirValues() {
+void printAirValues()
+{
   Serial.print("Temperature = ");
   Serial.print(bme.readTemperature());
   Serial.println(" *C");
-  
+
   // Convert temperature to Fahrenheit
   /*Serial.print("Temperature = ");
   Serial.print(1.8 * bme.readTemperature() + 32);
   Serial.println(" *F");*/
-  
+
   Serial.print("Pressure = ");
   Serial.print(bme.readPressure() / 100.0F);
   Serial.println(" hPa");
@@ -118,15 +69,17 @@ void printAirValues() {
   Serial.println(" %");
 }
 
-void printMoistMeter(){
+void printMoistMeter()
+{
   sensor_analog = analogRead(sensor_pin);
-  _moisture = ( 100 - ( (sensor_analog/4095.00) * 100 ) );
+  _moisture = (100 - ((sensor_analog / 4095.00) * 100));
   Serial.print("Moisture = ");
-  Serial.print(_moisture);  /* Print Temperature on the serial window */
+  Serial.print(_moisture); /* Print Temperature on the serial window */
   Serial.println("%");
 }
 
-void printUltraSonicValues(){
+void printUltraSonicValues()
+{
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
@@ -136,7 +89,7 @@ void printUltraSonicValues(){
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculate the distance
-  distanceCm = duration * SOUND_SPEED/2;
+  distanceCm = duration * SOUND_SPEED / 2;
   // Convert to inches
   distanceInch = distanceCm * CM_TO_INCH;
   // Prints the distance in the Serial Monitor
@@ -144,4 +97,79 @@ void printUltraSonicValues(){
   Serial.println(distanceCm);
   Serial.print("Distance (inch): ");
   Serial.println(distanceInch);
+}
+
+void checkPump()
+{
+  sensor_analog = analogRead(sensor_pin);
+  _moisture = (100 - ((sensor_analog / 4095.00) * 100));
+  if (_moisture > 25)
+  {
+    digitalWrite(25, HIGH);
+  }
+  else
+  {
+    digitalWrite(25, LOW);
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // Initialize the I2C bus for Light measuring
+  Wire.begin(21, 22); // SDA21, SCL=22
+
+  // setup for air measuring
+  bool status;
+  status = bme.begin(0x76);
+
+  // Light measurre init Log
+  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE))
+  {
+    // Serial.println(F("BH1750 initialized"));
+  }
+  else
+  {
+    Serial.println(F("Error initializing BH1750"));
+    while (1)
+      ;
+  }
+
+  // Air measure error Log
+  if (!status)
+  {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1)
+      ;
+  }
+
+  // Ultrasonic Setup
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+
+  pinMode(25, OUTPUT);
+}
+
+void loop()
+{
+
+  // Print Light Value
+  printLightValues();
+
+  // Print Air Value
+  printAirValues();
+
+  // Moist Meter
+  printMoistMeter();
+
+  // Ultrasonic
+  printUltraSonicValues();
+
+  checkPump();
+
+  Serial.println();
+
+  // digitalWrite(25,HIGH);
+  delay(2000);
 }
---
+++ final-blynk_ver/src/main.cpp
@@ -0,0 +1,127 @@
+/*************************************************************
+
+  This is a simple demo of sending and receiving some data.
+  Be sure to check out other examples!
+ *************************************************************/
+
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID "TMPL6DRQnlCVV"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "N2RLZVdZPFcRUmLivGbA-RGi4aY1ssoc"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "thamva";
+char pass[] = "123456789";
+
+// Moist Meter
+int _moisture, sensor_analog;
+int sensor_pin = 15;
+
+// Ultrasonic
+const int trigPin = 5;
+const int echoPin = 18;
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+long duration;
+float distanceCm;
+float distanceInch;
+
+BlynkTimer timer;
+
+void printUltraSonicValues()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED / 2;
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+}
+
+void printMoistMeter()
+{
+  sensor_analog = analogRead(sensor_pin);
+  _moisture = (100 - ((sensor_analog / 4095.00) * 100));
+  Serial.print("Moisture = ");
+  Serial.print(_moisture); /* Print Temperature on the serial window */
+  Serial.println("%");
+}
+
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V0)
+{
+  // Set incoming value from pin V0 to a variable
+  int value = param.asInt();
+
+  // Update state
+  Blynk.virtualWrite(V1, value);
+  while (value == 1)
+  {
+    printUltraSonicValues(); 
+    Blynk.virtualWrite(V4, 30-distanceCm);
+    printMoistMeter();
+    delay(2000);
+  }
+}
+
+// This function is called every time the device is connected to the Blynk.Cloud
+BLYNK_CONNECTED()
+{
+  // Change Web Link Button message to "Congratulations!"
+  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+  Blynk.setProperty(V3, "onImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
+
+// This function sends Arduino's uptime every second to Virtual Pin 2.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V2, millis() / 1000);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  // Ultrasonic Setup
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // You can also specify server:
+  // Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  // Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
